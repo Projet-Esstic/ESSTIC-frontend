1fdf7cedc2288fc68cd68ff0e5eb6291ca1c083e
Rean64 <somorean433@gmail.com>
2025-03-04 14:16:17 +0100

    design done for login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -6,7 +6,7 @@
         <img 
           src="@/assets/images/esstic-logo.png" 
           alt="School Campus" 
-          class="mx-auto mb-4  object-contain rounded-lg shadow-2xl transform transition hover:scale-105"
+          class="mx-auto mb-4 object-contain rounded-lg shadow-2xl transform transition hover:scale-105"
         />
         <h1 class="text-2xl md:text-4xl font-bold text-gray-800 mb-3 md:mb-4">
           ESSTIC School Portal
@@ -104,12 +104,25 @@
               </div>
               <input 
                 id="password"
+                :type="showPassword ? 'text' : 'password'"
                 v-model="password"
-                type="password" 
                 placeholder="Enter your password" 
                 required 
-                class="w-full pl-8 md:pl-10 px-3 md:px-4 py-2 text-sm md:text-base border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                class="w-full pl-8 md:pl-10 pr-10 px-3 md:px-4 py-2 text-sm md:text-base border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
+              <button 
+                type="button" 
+                @click="togglePasswordVisibility"
+                class="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-400 hover:text-blue-500 focus:outline-none"
+              >
+                <svg v-if="!showPassword" class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"></path>
+                </svg>
+                <svg v-else class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
+                </svg>
+              </button>
             </div>
           </div>
           
@@ -166,7 +179,8 @@ export default {
       email: '',
       password: '',
       rememberMe: false,
-      errorMessage: ''
+      errorMessage: '',
+      showPassword: false
     };
   },
   methods: {
@@ -177,6 +191,9 @@ export default {
       } catch (error) {
         this.errorMessage = error.message;
       }
+    },
+    togglePasswordVisibility() {
+      this.showPassword = !this.showPassword;
     }
   }
 };
